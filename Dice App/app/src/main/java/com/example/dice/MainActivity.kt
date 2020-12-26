9adf94281a06cd6c6606a761a4e89743afa3c2b1
venkatasridharsaikalavalapalli <kv.sridharsai@gmail.com>
2020-12-26 23:34:29 +0530

    Second commit

--- Dice App/app/src/main/java/com/example/dice/MainActivity.kt
+++ Dice App/app/src/main/java/com/example/dice/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
+/**
+ * this activity allows the uset to roll the dice and see the result on the screen
+ *
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,18 +17,35 @@ class MainActivity : AppCompatActivity() {
         //creating a button object here
         val rolldice:Button = findViewById(R.id.button)
 
-        /*
-        Android automatically assigns ID numbers to the resources in your app.
-        For example, the Roll button has a resource ID, and the string for the button text also has a resource ID.
-        Resource IDs are of the form R.<type>.<name>; for example, R.string.roll. For View IDs, the <type> is id,
-        for example, R.id.button.
+        /**
+         * short notes on how the id's in android are assigned
+         * Android automatically assigns ID numbers to the resources in your app.
+         * For example, the Roll button has a resource ID, and the string for the button text also has a resource ID.
+         *  Resource IDs are of the form R.<type>.<name>; for example, R.string.roll. For View IDs, the <type> is id,
+         * for example, R.id.button.
         */
 
         //adding an action listener to make the button work
 
         rolldice.setOnClickListener{
-            val toast = Toast.makeText(this,"Dice Rolled!",Toast.LENGTH_SHORT)
-            toast.show()
+            roll_dice_action()
         }
+    }
+
+    /**
+     * this function contains the logic to roll the dice and display the result on the screen
+     */
+    private fun roll_dice_action() {
+        val generate = Dice(6)
+        val diceroll = generate.rolldice()
+
+        val resultTextView : TextView = findViewById(R.id.textView)
+        resultTextView.text = diceroll.toString()
+    }
+}
+
+class Dice(private val no_of_sides:Int){
+    fun rolldice():Int{
+        return (1..no_of_sides).random()
     }
 }
