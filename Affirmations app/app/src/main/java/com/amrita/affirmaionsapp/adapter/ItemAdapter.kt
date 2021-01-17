f73ff9f7319cfde59057de32ebf2d890443c53c7
venkatasridharsaikalavalapalli <kv.sridharsai@gmail.com>
2021-01-17 22:22:54 +0530

    affirmations

--- Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/adapter/ItemAdapter.kt
+++ Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/adapter/ItemAdapter.kt
@@ -1,8 +1,14 @@
 package com.amrita.affirmaionsapp.adapter
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.amrita.affirmaionsapp.R
 import com.amrita.affirmaionsapp.model.Affirmations
 
 class ItemAdapter(private val context: Context,private val dataset: List<Affirmations>) {
-
+    class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
+        val textview:TextView = view.findViewById(R.id.item_title)
+    }
 }
