593c443e962afd12fe174dd8a49c011126a6a11d
venkatasridharsaikalavalapalli <kv.sridharsai@gmail.com>
2021-01-21 00:08:01 +0530

    Affirmations app Polishing left

--- Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/MainActivity.kt
+++ Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.amrita.affirmaionsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.amrita.affirmaionsapp.adapter.ItemAdapter
+import com.amrita.affirmaionsapp.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val mydataset = DataSource().loadAffirmations()
+        val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recyclerview.adapter = ItemAdapter(this,mydataset)
+        recyclerview.setHasFixedSize(true)
 
     }
 }
--- Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/adapter/ItemAdapter.kt
+++ Affirmations app/app/src/main/java/com/amrita/affirmaionsapp/adapter/ItemAdapter.kt
@@ -1,14 +1,39 @@
 package com.amrita.affirmaionsapp.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.amrita.affirmaionsapp.R
 import com.amrita.affirmaionsapp.model.Affirmations
 
-class ItemAdapter(private val context: Context,private val dataset: List<Affirmations>) {
+class ItemAdapter(private val context: Context,private val dataset: List<Affirmations>):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view:View):RecyclerView.ViewHolder(view){
+        val imageView:ImageView = view.findViewById(R.id.image)
         val textview:TextView = view.findViewById(R.id.item_title)
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textview.text = context.resources.getString(item.stringResourceId)
+    }
+
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    override fun getItemCount() = dataset.size
 }
